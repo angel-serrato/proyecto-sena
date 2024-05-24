ecae118724a930ae71df219017fd1e6411a7db64
Angel Serrato <serratoarias@yahoo.com>
2024-05-23 22:02:38 -0500

    Organizando proyecto la pizza de el canas

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # La Pizzería del Canas
 
 Análisis y Desarrollo de Software SENA
+
+https://www.youtube.com/watch?v=OVESuyVoPkI
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,19 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'First Web Page' })
+    res.render('index', { title: 'La Pizza de el Canas' })
 });
 
 router.get('/about', (req, res) => {
-    res.render('about', { title: 'About me' })
+    res.render('about', { title: 'About Me' })
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact', { title: 'Contact me' })
+    res.render('contact', { title: 'Contact Me' })
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Log In' })
 });
 
 export default router;
